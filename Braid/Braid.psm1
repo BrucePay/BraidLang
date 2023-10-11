68ffb5b008daf61a293749155e172d2dc42b4e06
StartAutomating <StartAutomating@github.com>
2023-10-11 05:05:13 +0000

    Adding Braid.ps.psm1 (Fixes #8)

---
+++ Braid/Braid.psm1
@@ -0,0 +1,12 @@
+$CommandsPath = Join-Path $PSScriptRoot "Commands"
+:ToIncludeFiles foreach ($file in (Get-ChildItem -Path "$CommandsPath" -Filter "*-*" -Recurse)) {
+    if ($file.Extension -ne '.ps1')      { continue }  # Skip if the extension is not .ps1
+    foreach ($exclusion in '\.[^\.]+\.ps1$') {
+        if (-not $exclusion) { continue }
+        if ($file.Name -match $exclusion) {
+            continue ToIncludeFiles  # Skip excluded files
+        }
+    }     
+    . $file.FullName
+}
+
