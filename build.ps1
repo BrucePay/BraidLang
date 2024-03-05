413c97a5da71cdd3d1e1f0abdfa7f10439445721
James Brundage <@github.com>
2024-03-05 11:12:48 -0800

    fix: Staginf more files in Braid build ( re #2, re #4 )

--- build.ps1
+++ build.ps1
@@ -14,52 +14,55 @@ param (
 
 $ErrorActionPreference = "stop"
 
-if (-not $NonCore) {
-    dotnet build (Join-path "src" "BraidCore.csproj")
-    return
-}
-
 $StageDir = Join-Path $PSScriptRoot "stage"
-
-; msbuild properties
-$properties = ""
-
-if ($Optimize)
-{
-    $properties += "Configuration=release"
-}
-else
-{
-    $properties += "Configuration=debug"
-}
-
-if ($clean)
-{
-    msbuild "-t:clean" "-p:$properties"  .\src\braidlang.csproj
-}
-
-msbuild "-p:$properties"  .\src\braidlang.csproj
-
-if ($LASTEXITCODE)
-{
-    write-host "? is $?"
-    throw "Build failed with exit code $LASTEXITCODE."
-}
 
 if (-not (Test-Path $StageDir))
 {
     $madeStagingDirectory = mkdir $StageDir
 }
 
+if (-not $NonCore) {
+    dotnet build (Join-path "src" "BraidCore.csproj")    
+} else {    
+    $properties = ""
+    
+    if ($Optimize)
+    {
+        $properties += "Configuration=release"
+    }
+    else
+    {
+        $properties += "Configuration=debug"
+    }
+    
+    if ($clean)
+    {
+        msbuild "-t:clean" "-p:$properties"  .\src\braidlang.csproj
+    }
+    
+    msbuild "-p:$properties"  .\src\braidlang.csproj
+    
+    if ($LASTEXITCODE)
+    {
+        write-host "? is $?"
+        throw "Build failed with exit code $LASTEXITCODE."
+    }
+    
+    
+    
+    
+    
+}
+
 if ($Optimize)
 {
-    cp src/bin/Release/*.* $StageDir   
+    Copy-Item src/bin/Release/*.* $StageDir   
 }
 else
 {
-    cp src/bin/Debug/*.* $StageDir   
+    Copy-Item src/bin/Debug/*.* $StageDir   
 }
 
-cp -verbose src/BraidRepl.ps1 $StageDir
-cp -verbose src/*.tl   $StageDir
 
+Copy-Item -verbose src/BraidRepl.ps1 $StageDir -PassThru
+Copy-Item -verbose src/*.tl   $StageDir -PassThru
